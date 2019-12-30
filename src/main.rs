0690e88ad1d876896c4435d7f924745e2971d40e
keiv.fly <kv>
2019-12-30 15:31:18 +0100

    wo lalrpop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use regex::Regex;
+
+pub enum Type {
+    I32(i32),
+    F64(f64),
+    Str(String),
+}
+
 fn main() {
+    let q_re = Regex::new("\"(\\.|[^\"])*\"").unwrap();
+    let s = String::from("a = 1");
+    let q_count = s.matches("\"").count();
+    // let s_wo_q = if q_count == 0 {
+    //     s
+    // } else {
+    //     s.split('\"').step_by(2).collect()
+    // };
+    let s_wo_q = s.split('\"').step_by(2).collect();
+    println!("{}", s_wo_q);
     println!("Hello, world!");
+    println!("hello2");
 }
